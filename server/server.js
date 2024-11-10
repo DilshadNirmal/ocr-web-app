c3e23471d869e34d5d336fad60aaba89c4b9ffc5
Dilshad Nirmal <dilshadgov2020@gmail.com>
2024-11-10 18:20:47 +0530

    server port handled

--- server/server.js
+++ server/server.js
@@ -48,7 +48,7 @@ app.post('/extract', (req, res) => {
   res.json(extractedData);
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
